9f51ae1492c06dfb39a5d620eb39e5b8c452a4fe
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2020-04-21 18:21:17 +0000

    Update mailsender.py

--- mailsender.py
+++ mailsender.py
@@ -1,30 +1,9 @@
 from smtplib import SMTP
 import email.message
-import sys, getopt
+import sys
 
 def sendmailg(gserver , gport ,guser, gpass , mailfrom , mailto , subject ,msg , ishtml = 0):
-    print(gserver + gport + guser + gpass + mailfrom + mailto + msg )
-    if gserver == '' :
-        print('you must set the parameter server .')
-        sys.exit(1)
-    elif gport == '' :
-        print('you must set the parameter port .')
-        sys.exit(1)
-    elif mailfrom == '' :
-        print('you must set the parameter mailfrom .')
-        sys.exit(1)
-    elif mailto == '' :
-        print('you must set the parameter mailto .')
-        sys.exit(1)
-    elif subject == '' :
-        print('you must set the parameter subject .')
-        sys.exit(1)
-    elif msg == '' :
-        print('you must set the parameter msg .')
-        sys.exit(1)
-    else:
-        print(gserver + gport + guser + gpass + mailfrom + mailto + msg )
-
+    #print(gserver + gport + guser + gpass + mailfrom + mailto + msg )
     mailmsg = email.message.Message()
     mailmsg['From'] = mailfrom
     mailmsg['To'] = mailto
@@ -35,7 +14,7 @@ def sendmailg(gserver , gport ,guser, gpass , mailfrom , mailto , subject ,msg ,
     else:
         mailmsg.add_header('Content-Type','text/plain')
 
-    mailmsg.set_payload (msg, 'utf-8')
+    mailmsg.set_payload (msg)
     with SMTP(gserver , gport)  as smtp:
         smtp.ehlo()
         smtp.starttls()
@@ -44,50 +23,55 @@ def sendmailg(gserver , gport ,guser, gpass , mailfrom , mailto , subject ,msg ,
         smtp.sendmail(mailfrom, mailto,mailmsg.as_string())
         smtp.quit()
 
-def ghelp():
-    print ('test.py -s --server -p --port -u --user -k --password -f --mailfrom -t --mailto -d --subject -m --messege')
-    sys.exit()
-
 def main(argv):
-    gserver = ''
-    gport = ''
-    guser = ''
-    gpass = ''
-    mailfrom = ''
-    mailto = ''
-    subject = ''
-    msg = ''
-    if len(argv) > 0:
-        opts, args = getopt.getopt(argv,"hs:p:u:k:f:t:d:m:",["help","server=","port=","user=","password=","mailfrom=","mailto=","subject=","messege="])
-    else:
-        ghelp()
-    
-    for opt, arg in opts:
-        print('arg = '+ arg + ' opt = '+ opt)
-        if opt in ('-h', "--help"):
-            ghelp()
-        elif opt in ("-s", "--server"):
-            gserver = arg
-        elif opt in ("-p", "--port"):
-            gport = arg
-        elif opt in ("-u", "--user"):
-            guser = arg
-        elif opt in ("-k", "--password"):
-            gpass = arg
-        elif opt in ("-f", "--mailfrom"):
-            mailfrom = arg
-        elif opt in ("-t", "--mailto"):
-            mailto = arg
-        elif opt in ("-d", "--subject"):
-            subject = arg
-        elif opt in ("-m", "--messege"):
-            msg = arg
+    W  = '\033[0m'  # white (normal)
+    R  = '\033[31m' # red
+    G  = '\033[32m' # green
+    O  = '\033[33m' # orange
+    B  = '\033[34m' # blue
+    P  = '\033[35m' # purple
+    print('\t{}Welcome to mail sender framework ......{}'.format(G,G))
+    print('\t{}you can use this framework for sending mails to a single email or email list{}'.format(R,R))
+    print('\t{}1- Single Email . {}'.format(O,O))
+    print('\t{}2- Multi Emails in file . {}'.format(O,O))    
+    sendtype = -1
+    while True:
+        sendtype = int(input('\t{0}Enter Your choice : {1}'.format(B,B)))
+        if sendtype > 2 or sendtype < 0 :
+            print('{}\tYou Entered a wrong choice please insert a valid one .{}'.format(R,R))
         else:
-            sys.exit(1)
-        
-    sendmailg(gserver , gport , guser, gpass , mailfrom , mailto , subject ,msg , 1)
+            if sendtype == 1:
+                print('{}\tyou choosed {} then you want send mail for single mail{}'.format(R,sendtype,R))
+            else:
+                print('{}\tyou choosed {} then you want send mail for Multi email from file{}'.format(R,sendtype,R))
+            
+            break
+    gserver = input('\t{0}Enter The Smtp Server : {1}'.format(B,B))
+    gport = int(input('\t{0}Enter The Smtp port : {1}'.format(B,B)))
+    guser = input('\t{0}Enter The Smtp User : {1}'.format(B,B))
+    gpass = input('\t{0}Enter The Smtp password : {1}'.format(B,B))
+    mailfrom = input('\t{0}Enter The sender email : {1}'.format(B,B))
+    if sendtype == 1 :
+        mailto = input('\t{0}Enter The reciver email : {1}'.format(B,B))
+    else:
+        mailto = input('\t{0}Enter The recivers emails file : {1}'.format(B,B))
+    subject = input('\t{0}Enter The Subject : {1}'.format(B,B))
+    msg = input('\t{0}Enter The mail Body : {1}'.format(B,B))
+    while True:
+        ishtml = input('\t{0}is the message is html yes or no: {1}'.format(B,B))
+        if ishtml == 'yes':
+            intishtml = 1
+            break
+        elif ishtml == 'no' :
+            intishtml = 0
+            break
+    if sendtype == 1 :
+        sendmailg(gserver , gport , guser, gpass , mailfrom , mailto , subject ,msg , intishtml)
+    else:
+        f = open(mailto,'r')
+        with line in f:
+            sendmailg(gserver , gport , guser, gpass , mailfrom , line , subject ,msg , intishtml)
     
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
